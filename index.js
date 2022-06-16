e537f37ba736d5136e94e40915b1c151f94f94ab
Leilani Migisi <leilani.migisi@student.moringaschool.com>
2022-06-16 08:24:00 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-const dodger = document.getElementById("dodger");
+var dodger = document.getElementById('dodger');
 
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
   }
-document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  });
+}
+
 function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-
-    if ( left < 200){
-        dodger.style.left = `${left + 1}px`;
-    }
-}
-document.addEventListener ("keydown" , function (e){
-    if (e.key === "Arrowleft"){
-        moveDodgerLeft();
-    }
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`
+  }
 }
 
-)
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
